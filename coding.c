752a5a6bd0f0ad33957681ffdcaf0042e62a2eea
HYT <858686513@qq.com>
2022-01-05 20:22:21 +0800

    链表删除元素-修改

--- coding.c
+++ coding.c
@@ -7,19 +7,20 @@ LeetCode_203 removeLinkListElem
 typedef struct LinkNode{
     int val;
     LinkNode *next;
-}Linklist,LinkNode;
+}LinkNode;
 
-Linklist *removeElements(Linklist* head, int val){      //移除数组元素
+LinkNode* removeElements(LinkNode* head, int val){      //移除数组元素
     LinkNode *tHead = (LinkNode*)malloc(sizeof(LinkNode));
     tHead->next = head;
-    while(tHead->next != NULL){
-        if(tHead->next->val == val){
-            LinkNode *temp = tHead->next;
-            tHead->next = temp->next;
+    LinkNode *cur = tHead;
+    while(cur->next != NULL){
+        if(cur->next->val == val){
+            LinkNode *temp = cur->next;
+            cur->next = temp->next;
             free(temp);
         }
         else{
-            tHead = tHead->next;
+            cur = cur->next;
         }
     }
     head = tHead->next;
@@ -27,29 +28,32 @@ Linklist *removeElements(Linklist* head, int val){      //移除数组元素
     return head;
 }
 
-void InitLinkList(Linklist *head,int val){     //初始化数组
-    head = (LinkNode*)malloc(sizeof(LinkNode));
-    head->val = val;
-    head->next = NULL;
-}
+// LinkNode* InitLinkList(LinkNode* head,int val){     //初始化数组
+//     head = (LinkNode*)malloc(sizeof(LinkNode));
+//     head->val = val;
+//     head->next = NULL;
+//     return head;
+// }
 
-void InsertLinkList(Linklist *head, int val){       //尾插法插入数组   
+LinkNode* InsertLinkList(LinkNode* head, int val){       //尾插法插入数组   
     LinkNode *t = (LinkNode*)malloc(sizeof(LinkNode));
     t->val = val;
+    t->next = NULL;
     LinkNode *temp = head;
-    while (temp->next != NULL)
-    {
+    while(temp->next != NULL){
         temp = temp->next;
     }
     temp->next = t;
-    t->next = NULL;
+    return head;
 }
 
 int main(){
     int arr[7] = {1,2,6,3,4,5,6};   //创建一个数组实例
-    Linklist *head;
-    InitLinkList(head,1);
-    for(int i = 0;i < 7; ++i){      //将数组元素存入链表
+    LinkNode *head = (LinkNode*)malloc(sizeof(LinkNode));                 //头指针
+    // InitLinkList(head,1);
+    head->val = 1;
+    head->next = NULL;
+    for(int i = 1;i < 7; ++i){      //将数组元素存入链表
         InsertLinkList(head,arr[i]);
     }
     removeElements(head,6);
